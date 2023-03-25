c41d16d3bb41a46b17dd1d45aba3272b4eefa471
Mohamed Yassine Boukhari <mohamed.boukhari@epfl.ch>
2023-03-25 18:33:21 +0100

    smc_party v2 not tested, still needs beaver

--- smc_party.py
+++ smc_party.py
@@ -59,13 +59,54 @@ class SMCParty:
 
         #TODO modify this for the first participant , check with TAs on how to 
         self.is_first_party = False
+        self.local_shares = self.share_secrets()
+
+
+
+    def share_secrets(self) -> Dict[str,Share]:
+        num_participants = len(self.protocol_spec.participant_ids)
+        local_shares = {} 
+        
+        for key,value in self.value_dict.items():
+            shares = share_secret(value, num_participants)
+
+            shares_and_ids = zip(shares, self.protocol_spec.participant_ids)
+
+            for share, id in shares_and_ids:
+                if id == self.client_id:
+                    local_shares[key.id] = share
+                else:
+                    self.comm.send_private_message(id, key.id, share.serialize())
+        
+        return local_shares
+
+
+
 
 
     def run(self) -> int:
         """
         The method the client use to do the SMC.
         """
-        raise NotImplementedError("You need to implement this method.")
+
+        expr = self.protocol_spec.expr
+
+        # Evaluate Expression
+        local_share = self.process_expression(expr)
+
+        # Share evaluation with other parties
+        label = 'client_id:'  + self.client_id + ',expression_id:' + expr.id 
+        to_send = local_share.serialize()
+        self.comm.publish_message(label,to_send)
+
+        shares = [local_share]
+        # Retrieve other shares
+        for pid in [pid for pid in self.protocol_spec.participant_ids if pid != self.client_id]:
+            label = 'client_id:'  + pid + ',expression_id:' + expr.id
+            pid_share = Share.deserialize(self.comm.retrieve_public_message(pid,label))
+            shares.append(pid_share)
+
+        return reconstruct_secret(shares)
 
 
     # Suggestion: To process expressions, make use of the *visitor pattern* like so:
@@ -74,15 +115,13 @@ class SMCParty:
             expr: Expression
         ):
 
-        #Here we need to call the corresponding protocol to the operations , first we can treat scalars as secrets , then optimize
-        #after we know that the implementation works
         p = self.process_expression
         
         if isinstance(expr, AddOp):
             return p(expr.a) + p(expr.b)
         
         elif isinstance(expr,AddKOp):
-            if self.client_id == self.is_first_party:
+            if self.is_first_party:
                 k = Share(expr.b.value)
             else:
                 k = Share()
@@ -101,8 +140,11 @@ class SMCParty:
             return Share(expr.value)
         
         elif isinstance(expr,Secret):
-            #TODO What to do when its a secret , retrieve secret share 
-            pass
+            if expr.id not in self.local_shares.keys():
+                share = Share.deserialize(self.comm.retrieve_private_message(expr.id))
+                self.local_shares[expr.id] = share
+            return self.local_shares[expr.id]
+
 
 
 
